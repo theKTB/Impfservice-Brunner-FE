0fb936fbbeef07e1d5d55a7480cbe4342ee01cb5
theKTB <s1810456006@students.fh-hagenberg.at>
2021-04-16 17:56:13 +0200

    2 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { VaccinationDetailsComponent } from "./vaccination-details/vaccination-details.component";
+import { VaccinationListComponent } from "./vaccination-list/vaccination-list.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomeComponent }
+  { path: "home", component: HomeComponent },
+  { path: "vaccinations", component: VaccinationListComponent },
+  { path: "vaccination/:id", component: VaccinationDetailsComponent }
 ];
 
 @NgModule({
