7dd01d1e3f160581040f43ffbbbdceffcdbfd060
theKTB <s1810456006@students.fh-hagenberg.at>
2021-04-16 17:47:35 +0200

    vaccination factory

---
+++ src/app/shared/vaccination-factory.ts
@@ -0,0 +1,22 @@
+import { Vaccination } from "./vaccination";
+
+export class VaccinationFactory {
+  static empty(): Vaccination {
+    //TODO: Kann ich hier bei der Location Null mitgeben, wenn das DB-Feld nicht nullable ist?
+    return new Vaccination(null, new Date(), new Date(), 0, null);
+  }
+
+  static fromObject(rawVaccination: any): Vaccination {
+    return new Vaccination(
+      rawVaccination.id,
+      typeof rawVaccination.from === "string"
+        ? new Date(rawVaccination.from)
+        : rawVaccination.from,
+      typeof rawVaccination.to === "string"
+        ? new Date(rawVaccination.to)
+        : rawVaccination.to,
+      rawVaccination.maxPatients,
+      rawVaccination.location_id
+    );
+  }
+}
