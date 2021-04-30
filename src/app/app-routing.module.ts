c3fcfb5cf14f4f307d59a73b9dcaaf6ce6d4c935
theKTB <s1810456006@students.fh-hagenberg.at>
2021-04-30 10:05:11 +0200

    login -part 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 import { VaccinationDetailsComponent } from "./vaccination-details/vaccination-details.component";
 import { VaccinationListComponent } from "./vaccination-list/vaccination-list.component";
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "vaccinations", component: VaccinationListComponent },
-  { path: "vaccinations/:id", component: VaccinationDetailsComponent }
+  { path: "vaccinations/:id", component: VaccinationDetailsComponent },
+  { path: "login", component: LoginComponent}
 ];
 
 @NgModule({
